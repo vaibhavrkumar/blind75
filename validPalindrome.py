d2c51f0bb0add5770f418968ab6104f4c008ff32
vaibhavrkumar <vaibhav_241298@yahoo.com>
2025-11-03 15:17:58 +0530

    naive+opt

---
+++ validPalindrome.py
@@ -0,0 +1,28 @@
+class Solution:
+    # print({self.isPalindrome("bob")})
+    def isPalindrome(self, s: str) -> bool:
+        strLen = len(s)
+        l, r = 0, strLen - 1
+
+        while l < r:
+            while l < r and not self.alphanum(s[l]):
+                l += 1
+            while r > l and not self.alphanum(s[r]):
+                r -= 1
+            if s[l].lower() != s[r].lower():
+                return False
+
+            l, r = l + 1, r - 1
+        return True
+
+    def alphanum(self, c):
+        return (
+            ord('A') <= ord(c) <= ord('Z')
+            or ord('a') <= ord(c) <= ord('z')
+            or ord('0') <= ord(c) <= ord('9')
+        )
+
+
+my_object = Solution()
+print(my_object.isPalindrome("race a car"))
+# print(my_object.alphanum(""))
